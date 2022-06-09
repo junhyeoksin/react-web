40400551ea14618b285e667772ad118105e3aee1
Jun Hyeok Shin <h-kun@naver.com>
2022-06-09 15:00:43 +0900

    Update: App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import HookUseEffect from "./HookUseEffect";
 import HookUseRef from "./HookUseRef";
+import Page from "./components/Page";
+import {useState} from "react";
+import {ThemeContext} from './context/ThemeContext';
+import {UserContext} from "./context/UserContext";
+
 function App() {
 
-
-
+  const [isDark, setIsDark] = useState(false);
 
   return (
     <>
-      {/* forEach 는 void  */}
-        <HookUseEffect/>
-        <HookUseRef/>
+
+      {/*  <HookUseEffect/>*/}
+      {/*  <HookUseRef/>*/}
+        <UserContext.Provider value={'사용자'}>
+            <ThemeContext.Provider value={{isDark, setIsDark}}>
+                <Page/>
+            </ThemeContext.Provider>
+        </UserContext.Provider>
+
     </>
   );
 }

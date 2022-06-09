5f8383999363ba13d2a16b378913cbbc1076ef3f
Jun Hyeok Shin <h-kun@naver.com>
2022-06-10 08:58:13 +0900

    Update: HookUseContext

---
+++ src/HookUseContext.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HookUseContext = ()  => {
+    /**
+     *  context : 컴포넌트를 재사용하기 어려워 질 수 있다.
+     *            prop drilling 을 피하기 위한 목적이라면 component Composition 을 고려.
+     */
+    return (
+        <div>
+            {/*useContext();*/}
+        </div>
+    );
+};
+
+export default HookUseContext;
